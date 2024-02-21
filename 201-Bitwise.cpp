92a1c464d936f6c625ac1b3b977b120f9a471226
abdulrafayl226825 <l226825@lhr.nu.edu.pk>
2024-02-21 21:04:21 +0500

    single function using sum

--- 201-Bitwise.cpp
+++ 201-Bitwise.cpp
@@ -1,54 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-string DecimalToBinaryString(int& a)
-{
-    string binary = "";
-    while (a > 0)
-    {
-        int rem = a % 2;
-        binary = to_string(rem) + binary;
-        a = a / 2;
-    }
-    return binary;
-}
-int BinaryStringToDecimal(const string& binary)
-{
-    int decimal = 0;
-    int power = 0;
-    for (int i = binary.length() - 1; i >= 0; i--)
-    {
-        if (binary[i] == '1')
-        {
-            decimal += pow(2, power);
-        }
-        else if (binary[i] != '0')
-        {
-            throw invalid_argument("Invalid binary string");
-
-        }
-        power++;
-    }
-    return decimal;
-}
 int rangeBitwiseAnd(int left, int right) {
-    if(left==right) return left;
 
 
-    auto x=DecimalToBinaryString(left);
-    auto y=DecimalToBinaryString(right);
-    int low=x.length();
-    int high=y.length();
-    if(high!=low) return 0;
-    string s(high,'0');
-    int i=0;
-    for(;i<=high;i++)
-    {
-        if(x[i]!=y[i])
-            break;
-        s[i]=x[i];
+        if(left == 0 || right == 0 )
+            return 0;
+
+        if (left == right)
+            return left;
+            
+        int powL = log2(right);
+        int powH = log2(left);
+        
+        if (powL != powH)
+            return 0;
+
+        long long int i = pow(2,powL);
+        int sum = 0;
+
+        while(sum + i <= left && sum + i <= right || sum + i > left && sum + i > right)
+        {
+            if (sum + i <= left)
+                sum += i;
+            i/= 2;
+        }
+
+        return sum;
     }
-    return BinaryStringToDecimal(s);
-}int main()
+
+int main()
 {
   int low =16;
       cout<<rangeBitwiseAnd(5,7)<<endl;
